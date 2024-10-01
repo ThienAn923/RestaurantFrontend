082a104f113c03fec705b430c380d35b63c154cc
ThienAn923 <ThienAn923>
2024-10-01 16:10:41 +0700

    change positionManagement a bit

--- src/components/PositionManagement.vue
+++ src/components/PositionManagement.vue
@@ -7,7 +7,7 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+// import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { usePositionStore } from './pinia/position.store'
 import { useEmployeeStore } from './pinia/employee.store'
 
